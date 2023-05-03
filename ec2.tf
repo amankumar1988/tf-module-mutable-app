00df52076fbc0cab06e46da14c62ae4ba9f72c55
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-03 23:08:06 +0530

    Auto Push

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,11 @@
 # Create spot request : based on availability , aws will grant EC2
 resource "aws_spot_instance_request" "spot" {
-  count                 = var.SPOT_INSTANCE_COUNT
-  ami                   = data.aws_ami.ami.id
-  instance_type         = var.INSTANCE_TYPE
-  wait_for_fulfillment  = true
+  count                     = var.SPOT_INSTANCE_COUNT
+  ami                       = data.aws_ami.ami.id
+  instance_type             = var.INSTANCE_TYPE
+  wait_for_fulfillment      = true
+  vpc_security_group_ids    = [aws_security_group.allow_app.id]
+  subnet_id                 = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}-spot"
@@ -13,10 +15,11 @@ resource "aws_spot_instance_request" "spot" {
 # Creates On-demand EC2
 
 resource "aws_instance" "od" {
-  count                 = var.OD_INSTANCE_COUNT
-  ami                   = data.aws_ami.ami.id
-  instance_type         = var.INSTANCE_TYPE
-
+  count                  = var.OD_INSTANCE_COUNT
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.INSTANCE_TYPE
+  vpc_security_group_ids = [aws_security_group.allow_app.id]
+  subnet_id                 = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}-od"
   }
--- vars.tf
+++ vars.tf
@@ -3,3 +3,4 @@ variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
 variable "OD_INSTANCE_COUNT" {}
+variable "APP_PORT" {}
