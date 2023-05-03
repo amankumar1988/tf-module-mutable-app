48bd7c6734585cb2a3d3a9fd3a1aafe20f059db4
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-03 23:01:48 +0530

    Auto Push

---
+++ sg.tf
@@ -0,0 +1,34 @@
+# Create security group
+
+resource "aws_security_group" "allow_app" {
+  name        = "robot-${var.COMPONENT}-${var.ENV}-sg"
+  description = "robot-${var.COMPONENT}-${var.ENV}-sg"
+  vpc_id      =  data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "Application Port"
+    from_port        = var.APP_PORT
+    to_port          = var.APP_PORT
+    protocol         = "tcp"
+    cidr_blocks      = data.terraform_remote_state.vpc.outputs.VPC_CIDR
+  }
+
+  ingress {
+    description      = "SSH Port"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = data.terraform_remote_state.vpc.outputs.VPC_CIDR
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "robot-${var.COMPONENT}-${var.ENV}-sg"
+  }
+}
