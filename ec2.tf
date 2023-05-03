4c2f268c8fb6c959a707f6062bdf436dd479a99d
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-03 22:55:50 +0530

    Auto Push

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,9 @@
 # Create spot request : based on availability , aws will grant EC2
 resource "aws_spot_instance_request" "spot" {
-  count         = var.SPOT_INSTANCE_COUNT
-  ami           = data.aws_ami.ami.id
-  instance_type = var.INSTANCE_TYPE
+  count                 = var.SPOT_INSTANCE_COUNT
+  ami                   = data.aws_ami.ami.id
+  instance_type         = var.INSTANCE_TYPE
+  wait_for_fulfillment  = true
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
