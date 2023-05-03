3d88ab2a5d68accdaddcefbde7ea79872c6a705d
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-03 22:57:45 +0530

    Auto Push

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,18 @@ resource "aws_spot_instance_request" "spot" {
   wait_for_fulfillment  = true
 
   tags = {
-    Name = "${var.COMPONENT}-${var.ENV}"
+    Name = "${var.COMPONENT}-${var.ENV}-spot"
+  }
+}
+
+# Creates On-demand EC2
+
+resource "aws_instance" "od" {
+  count                 = var.OD_INSTANCE_COUNT
+  ami                   = data.aws_ami.ami.id
+  instance_type         = var.INSTANCE_TYPE
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}-od"
   }
 }
--- vars.tf
+++ vars.tf
@@ -2,3 +2,4 @@ variable "ENV" {}
 variable "INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
+variable "OD_INSTANCE_COUNT" {}
