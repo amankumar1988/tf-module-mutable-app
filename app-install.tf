465e5db49295759048a5c4d852fd59258283f74e
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-04 12:59:54 +0530

    Auto Push

--- app-install.tf
+++ app-install.tf
@@ -1,19 +1,18 @@
 resource "null_resource" {
     count                     = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
 # Declaring the remote provisioner inside the resource
-  provisioner "remote-exec" {
-    connection {                  // establishes connectivity to the created machine
-    type     = "ssh"
-    user     = local.SSH_USERNAME
-    password = local.SSH_PASSWORD
-    host     = element(local.INSTANCE_IPS, count.index)
-  }
+    provisioner "remote-exec" {
+      connection {                  // establishes connectivity to the created machine
+      type     = "ssh"
+      user     = local.SSH_USERNAME
+      password = local.SSH_PASSWORD
+      host     = element(local.INSTANCE_IPS, count.index)
+    }
 
-    inline = [
-      "ansible-pull -U https://github.com/amankumar1988/ansible.git robot-pull.yaml -e ENV=dev -e COMPONENT=mongodb"
-    ]
-  }
-
+      inline = [
+        "ansible-pull -U https://github.com/amankumar1988/ansible.git robot-pull.yaml -e ENV=dev -e COMPONENT=${var.COMPONENT} -e VERSION=${var.APP_VERSION}"
+      ]
+    }
 }
 
 
