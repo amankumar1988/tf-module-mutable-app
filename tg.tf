64e6ac3ab348342fbe8f12fa6e8fb215c733e764
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 17:44:24 +0530

    Auto Push

--- tg.tf
+++ tg.tf
@@ -51,12 +51,7 @@ resource "aws_lb_listener" "public" {
   protocol          = "HTTP"
 
   default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
   }
 }
