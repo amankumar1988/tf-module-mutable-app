b4dd939f996170e78bd69b146a6e6bbc6e5682bf
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 15:35:19 +0530

    Auto Push

--- tg.tf
+++ tg.tf
@@ -16,7 +16,7 @@ resource "aws_lb_target_group_attachment" "attach_instance" {
 
 # Add rule
 resource "aws_lb_listener_rule" "app_rule" {
-  count        =  var.LAB_TYPE == "internal" ? 1 : 0
+  count        =  var.LB_TYPE == "internal" ? 1 : 0
 
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = random_integer.priority.result
