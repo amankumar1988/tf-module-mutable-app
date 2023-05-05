95f52f9e984e0b01cc12ba3f94860a5df78d0f62
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 14:43:02 +0530

    Auto Push

--- app-install.tf
+++ app-install.tf
@@ -15,29 +15,6 @@ resource "null_resource" "app" {
     }
 }
 
+# Provisioners are create time by default, which means they only run during the infrastructure creation or if the provisioner is a failure in the previous run.
+# In our case, if you've deployed 0.0.4 and wish to upgrade to 0.0.5, it won't run nor it work work.
 
-# # Create EC2
-# resource "aws_instance" "web" {
-#   ami                       = data.aws_ami.my_ami.image_id
-#   instance_type             = "t3.micro"
-#   vpc_security_group_ids    = [var.sg]
-
-#   tags = {
-#     Name        = "Terraform-Instance-Server-Name"
-#   }
-
-# # Declaring the remote provisioner inside the resource
-#   provisioner "remote-exec" {
-
-#     connection {                  // establishes connectivity to the created machine
-#     type     = "ssh"
-#     user     = "centos"
-#     password = "DevOps321"
-#     host     = self.private_ip
-#   }
-
-#     inline = [
-#       "ansible-pull -U https://github.com/amankumar1988/ansible.git robot-pull.yaml -e ENV=dev -e COMPONENT=mongodb"
-#     ]
-#   }
-# }
