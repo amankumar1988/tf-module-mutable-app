888ff16208fad067d53fe680af0fc88cb5bb3432
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 17:43:32 +0530

    Auto Push

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # tf-module-mutable-app
 
 If new Nexus EC2 created then change Pvt IP in - ansible > roles > common > tasks >download-from-nexus.yml and in cart > Jenkinsfile > env.NEXUS_URL="pvt_ip_of_nexus"
+also in common.groovy
--- tg.tf
+++ tg.tf
@@ -38,3 +38,25 @@ resource "random_integer" "priority" {
   min = 101
   max = 500
 }
+
+
+#Public ALB Listener
+
+# Creates a listener to the target group
+resource "aws_lb_listener" "public" {
+  count             = var.INTERNAL ? 1 : 0  
+
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LISTENER_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
