02c956a93aae81a5c44b416bc0c43abe93ff9c48
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 15:14:45 +0530

    Auto Push

--- tg.tf
+++ tg.tf
@@ -13,3 +13,22 @@ resource "aws_lb_target_group_attachment" "attach_instance" {
   target_id        = element(local.INSTANCE_IDS, count.index)
   port             = var.APP_PORT
 }
+
+# Add rule
+resource "aws_lb_listener_rule" "app_rule" {
+  count        =  var.LB_TYPE == "internal" ? 1 : 0
+
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority     = random_integer.priority.result
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"]
+    }
+  }
+}
