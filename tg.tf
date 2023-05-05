6e5d70f4b9e1db9f52ce87ddfd0569cc5941174f
amankumar1988 <kumar.aman6600@gmail.com>
2023-05-05 16:49:41 +0530

    Auto Push

--- tg.tf
+++ tg.tf
@@ -19,7 +19,7 @@ resource "aws_lb_listener_rule" "app_rule" {
   count        =  var.LB_TYPE == "internal" ? 1 : 0
 
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-#   priority     = random_integer.priority.result
+  priority     = random_integer.priority.result
 
   action {
     type             = "forward"
@@ -31,4 +31,10 @@ resource "aws_lb_listener_rule" "app_rule" {
       values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"]
     }
   }
+}
+
+# Generates a random integar
+resource "random_integer" "priority" {
+  min = 101
+  max = 500
 }
